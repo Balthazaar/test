f79db99b0ee55719d7114efffef4ad95f9ab03d3
root <root@localhost.localdomain>
2015-05-31 18:50:47 -0400

    second commit

--- inc/parser.inc.php
+++ inc/parser.inc.php
@@ -3,55 +3,61 @@
 class Parser {
 
 
-	public static function ParseUrl($url, $element){
+public static function ParseUrl($url, $element){
 
 
 
 	$html = file_get_html($url);
 
-
-      foreach ($html->find($element) as $find) {
-
-      	if ( is_array($find->children) ) {
-
-      		foreach ($find->children as $child) {
-      			
-      			echo $child.'<br>';
-
-      			if( !empty($child->href) ) {
-
-      		     echo 'href :'. $find->href.'<br>';
-          	    }
-
-         	  if( !empty($child->src) ) {
-
-      		   echo 'img src :'. $child->src.'<br>';
-      		   echo  "<img src='$url/$child->src'>";
-        	}
-      		}
-      	}
-
-       else {
-
-      	echo $find.'<br>';
-      }
-
-      	if( !empty($find->href) ) {
-
-      		echo 'href :'. $find->href.'<br>';
-      	}
-
-      	if( !empty($find->src) ) {
-
-      		echo 'img src :'. $find->src.'<br>';
-      		echo  "<img src='$url/$find->src'>";
-      	}
-	
-	
-     }
+	if ( !empty($html->find($element)) ) {
 
 
+		foreach ($html->find($element) as $find) {
 
-	
+			if ( is_array($find->children) ) {
+
+				foreach ($find->children as $child) {
+					
+					echo $child.'<br>';
+
+					if( !empty($child->href) ) {
+
+						echo 'href :'. $find->href.'<br>';
+					}
+
+					if( !empty($child->src) ) {
+
+						echo 'img src :'. $child->src.'<br>';
+						echo  "<img src='$url/$child->src'>";
+					}
+				}
+			}
+
+			else {
+
+				echo $find.'<br>';
+			}
+
+			if( !empty($find->href) ) {
+
+				echo 'href :'. $find->href.'<br>';
+			}
+
+			if( !empty($find->src) ) {
+
+				echo 'img src :'. $find->src.'<br>';
+				echo  "<img src='$url/$find->src'>";
+			}
+			
+			
+		}
 	}
+
+	else {
+
+		echo 'Nothing found';
+	}
+
+	
+}
 }
--- test query.sql
+++ test query.sql
@@ -1,19 +1,24 @@
-SELECT * FROM login_history 
+#Write a query which will find a user who had most logouts on Wednesdays in September 2012
+
+SELECT user_id FROM login_history 
 WHERE DAYNAME(login_time) = "Wednesday" 
 and MONTHNAME(login_time) = 'September' 
 and YEAR(login_time) = 2012
 and login_action=2 
-GROUP BY `user_id` 
+GROUP BY user_id
 ORDER BY COUNT(*) DESC LIMIT 1
 
+#Write a query which will find a user who had most logouts on monthly basis
+#Pass the month name and year from script to get the result?
 
-SELECT * FROM login_history 
+SELECT user_id FROM login_history 
 and MONTHNAME(login_time) = "$month" 
 and YEAR(login_time) = "$year"
 and login_action=2 
-GROUP BY `user_id` 
+GROUP BY user_id 
 ORDER BY COUNT(*) DESC LIMIT 1
 
+#Write a query which find total number of actions for every user_id
 
 SELECT user_id, 
 SUM(login_action = 1 ) AS total_logins,  
